abc250987fb2f87a36eec898fbb6311766622629
jinyue.jy <jinyue.jy@alibaba-inc.com>
2018-03-22 19:57:22 +0800

    test xxx

--- chizi/js/chizi.js
+++ chizi/js/chizi.js
@@ -6,9 +6,52 @@ var widthCM = screen.width * CMPerPix;
 console.log(widthCM)
 
 
-// mac:
+// macbook air:
 // 13.3" 1440*900
 // 127.68 PPI
 // 1cm = 0.393701 inch
 // 10cm = 10 * 0.393701 * 127.68
 // = 502.677165 px
+
+// iphone 6:
+// 4.7" 1334x750
+// 326ppi
+// 1inch = 325.6 pixels
+// 1mm = how many pixels? 1/10 * 0.393701 * 325.6
+// = 12.8 pixels 
+// 12.8 / 2 = 6.4
+// 1cm = 128 pixels. / 0.8 (经验) = 160pixels
+// 128 * 1.307 = 167
+
+// var cm = $('.mm').css({'width':'12px'})
+// var cm = $('.mid-cm').css({'width':'12px'})
+// var cm = $('.cm').css({'width':'12px'})
+
+
+var value = window.devicePixelRatio;
+
+alert($(window).width()) // 980
+alert($(window).height()) // 1445
+
+// alert(window.screen.width) //375 = 750
+// alert(window.screen.height) // 667 = 1334
+
+
+
+$(function(){
+//document.width / jQuery(document).width()
+// alert(document.width)
+// alert(jQuery(document).width())
+})
+
+// $(function() {
+//   console.log( "ready!" );
+//   var cm = document.getElementById('cm');
+//   var cmstyle = cm.style;
+//   console.log(cmstyle)
+//   console.log(cm.width())
+
+// });
+
+
+
